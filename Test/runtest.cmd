de4949d9f49084c7b9cc31abd9350f3d319d4989
Akira Kawamata <autumn@piedey.co.jp>
2018-10-14 11:43:28 +0900

    cut first line for test

--- Test/runtest.cmd
+++ Test/runtest.cmd
@@ -1,4 +1,6 @@
-dir ..\NBasicBin2Text\NBasicBin2Text.csproj
-dotnet run --project ..\NBasicBin2Text\NBasicBin2Text\NBasicBin2Text.csproj TSTBIN >result.txt
+dotnet run --project ..\NBasicBin2Text\NBasicBin2Text\NBasicBin2Text.csproj TSTBIN >temp.txt
+copy nul result.txt
+FOR /F "skip=1 tokens=* usebackq" %%i IN (temp.txt) DO @echo %%i>> result.txt
+del temp.txt
 fc result.txt tstasc
 pause
