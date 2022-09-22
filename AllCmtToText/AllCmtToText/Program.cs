3c21951f60d7bf5994cbca889ff81211e57c6b48
Akira Kawamata <autumn@piedey.co.jp>
2022-09-22 14:14:38 +0900

    add new

---
+++ AllCmtToText/AllCmtToText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("usage: AllCmdToText DIRECTORY");
+    return;
+}
+
+foreach (var fullpath in Directory.EnumerateFiles(args[0],"*.cmt", SearchOption.AllDirectories))
+{
+    byte[] data = File.ReadAllBytes(fullpath);
+    if (!data.Take(10).All(c => c == 0xd3))
+    {
+        Console.WriteLine($"Skipping: {fullpath}");
+        continue;
+    }
+    Console.WriteLine($"Processing: {fullpath}");
+    var outpath = Path.ChangeExtension(fullpath,"bas");
+    var p = Process.Start("nbasicbin2text", $"\"{fullpath}\" \"{outpath}\"");
+    p.WaitForExit();
+}
+Console.WriteLine("All Done");
+
+
+
+
